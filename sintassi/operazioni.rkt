57b825881c33d6d45c50ea4098d8e36c6d0c917f
Alex Duca <67121465+alexduca@users.noreply.github.com>
2021-11-12 20:05:14 +0100

    Operazioni Matematiche

---
+++ sintassi/operazioni.rkt
@@ -0,0 +1,30 @@
+ 
+; OPERAZIONI
+
+; (+ a b)
+(+ 1 2)       ; return 3
+
+; (- a b)
+(- 2 1)       ; return 1
+
+; (* a b)
+(* 2 2)       ; return 4
+
+; (/ a b)
+(/ 10 2)      ; return 5
+
+; ------------------------------------
+
+; OPERAZIONI A PIÙ FATTORI
+
+; (segno a b c ... n)
+(+ 1 2 3)     ; return 6
+
+; ------------------------------------
+
+; ESPRESSIONE A PIÙ OPERAZIONI
+
+; (a + b) + c   ->  (+ (+ a b) c)
+; es: (1 + 2) + 3
+(+ (+ 1 2) 3)   ; return 6
+ 
