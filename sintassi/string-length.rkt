1e4912041251454dfabfcf0adc31204b028460f1
Alex Duca <67121465+alexduca@users.noreply.github.com>
2021-11-12 20:07:34 +0100

    Lunghezza della stringa

---
+++ sintassi/string-length.rkt
@@ -0,0 +1,19 @@
+ 
+; STRING LENGTH
+; Calcola la lunghezza della stringa
+
+(string-append "casa")            ; return 4
+(string-append "benvenuto")       ; return 9
+
+; Esempio di una funzione con "string-length"
+
+(define lunghezza
+    (lambda (str)
+        (string-length str)
+    )
+)
+
+(lunghezza "ciao")                ; return 4
+(lunghezza "gatto")               ; return 5
+(lunghezza "benvenuto")           ; return 9
+ 
